41d12a5e6e97031eb82c376a4d291449ac84af7b
Georgio Harris <georgie.to.harris@gmail.com>
2023-05-14 14:53:55 -0500

    Completed the last two final tasks of the assignment, the stretch tasks. Also added html text asking a question about stetch task 4.g.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -108,11 +108,15 @@ console.log(topFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-topFoods
-console.log(topFoods.join(' and '));
+topFoods = topFoods.join(' and ');
+console.log(topFoods);
+console.log(`I love ${topFoods}`);
+
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+topFoodsAndAnimals = [topFoods, animalArray];
+console.log(topFoodsAndAnimals);
